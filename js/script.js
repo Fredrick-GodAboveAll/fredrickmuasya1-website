c35bb6815285b9d2c3733f7b12b5bb6242996d86
Fredrick Muasya <135801854+Fredrick-GodAboveAll@users.noreply.github.com>
2023-06-24 14:59:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,4 +30,31 @@ window.addEventListener('resize', () =>{
     resizeTimer = setTimeout(() => {
         document.body.classList.remove('resize-animation-stopper');
     }, 400);
+
+    // Get the tab links and tab content sections
+const tabLinks = document.querySelectorAll('.tab-links a');
+const tabContent = document.querySelectorAll('.tab-content .tab');
+
+// Add click event listener to each tab link
+tabLinks.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Remove 'active' class from all tab links and tab content
+    tabLinks.forEach(function(link) {
+      link.classList.remove('active');
+    });
+    tabContent.forEach(function(tab) {
+      tab.style.display = 'none';
+    });
+
+    // Add 'active' class to clicked tab link
+    this.classList.add('active');
+
+    // Show the corresponding tab content
+    const targetTab = document.querySelector(this.getAttribute('href'));
+    targetTab.style.display = 'block';
+  });
+});
+
 });
